db885e762ada46d50729f4a1f4c33b4a05b23c40
harshit-kms <158085343+harshit-kms@users.noreply.github.com>
2024-06-09 16:44:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const key = "d1226a2dd284b0213a51858a658d3763";
+const searchBar = document.querySelector(".location-bar input");
+const button = document.querySelector(".location-bar button");
+  
+async function weatherForecast(cityName){
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}&units=metric`;
+    const response = await fetch(URL);
+
+    if(response.status === 404){
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    }
+    else{
+        const data = await response.json();
+        // console.log(data);
+        document.querySelector(".location").innerText = data.name; 
+        document.querySelector(".temp").innerText = Math.round(data.main.temp) + " °C"; 
+        document.querySelector(".humidity .details-text p").innerText = data.main.humidity + "%"; 
+        document.querySelector(".wind .details-text p").innerText = data.wind.speed + " mph"; 
+        document.querySelector(".weather img").src = `images/${data.weather[0].main.toLowerCase()}.png`;
+
+
+    } 
+
+
+}
+
+weatherForecast("new york");
+
+button.addEventListener("click", () => {
+    weatherForecast(searchBar.value);
+});
+
+searchBar.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        weatherForecast(searchBar.value);
+    }
+});
